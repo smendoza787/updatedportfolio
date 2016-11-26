3733a7ea73c7bb2672fcce23e1ca6bec3177f720
Sergio Mendoza <smendoza787@gmail.com>
2016-11-26 02:45:43 -0800

    configured home, about, projects and blog page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'pages#home'
+  get 'about', to: 'pages#about'
+  get 'projects', to: 'pages#projects'
+  get 'blog', to: 'pages#blog'
 end
